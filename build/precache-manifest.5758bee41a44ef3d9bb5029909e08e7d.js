5e1df2ae60c28a8c20d221bab79eef4cae57691b
Mina <mina.mounir@hotmail.com>
2019-04-11 18:11:18 +0300

    first commit

---
+++ build/precache-manifest.5758bee41a44ef3d9bb5029909e08e7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b9d27b1f6a4dbfbfc774",
+    "url": "/static/js/main.baba92b5.chunk.js"
+  },
+  {
+    "revision": "6519de9870142d856557",
+    "url": "/static/js/2.ca1b9fc6.chunk.js"
+  },
+  {
+    "revision": "b9d27b1f6a4dbfbfc774",
+    "url": "/static/css/main.d4e41cdc.chunk.css"
+  },
+  {
+    "revision": "ca6469ebf2a4c6ad5e64764d4e4e3644",
+    "url": "/index.html"
+  }
+];
